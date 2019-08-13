76ead2836fc0636ea1b78dc9d977525cf9d35177
Victor Reinor <victorreinor@gmail.com>
2019-08-12 21:22:34 -0300

    adiciona layous padroes que serao utilizadas em varios lugares

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '../pages/SignIn';
-import SignUp from '../pages/SignUP';
+import SignUp from '../pages/SignUp';
 
 import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
