8aba05144b2d3c777f0afbe6a047d8e1046901cc
Victor Reinor <victorreinor@gmail.com>
2019-08-16 19:38:08 -0300

    adiciona o loading no autenticacao e action para  verificar falha no login

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistedReducer = persistReducer(
     {
-      key: 'gobarbar',
+      key: 'gobarber',
       storage,
       whitelist: ['auth', 'user'],
     },
