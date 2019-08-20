76c3b1aa02bebe852071450051618496fb03627b
Victor Reinor <victorreinor@gmail.com>
2019-08-19 21:05:04 -0300

    adiciona formulario para alterar o perfil do usuario logado

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input type="name" name="name" placeholder="Nome Completo" />
+        <Input type="email" name="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Confirmação de senha"
+        />
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
   );
 }
 
