2bd11977752b955f2c0977f94ceb2693a2302946
Victor Reinor <victorreinor@gmail.com>
2019-08-21 20:49:42 -0300

    adiciona as informacoes ao header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +9,8 @@ import logo from '~/assets/logoRoxa.svg';
 import { Container, Content, Profile } from './styles';
 
 function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -20,12 +23,15 @@ function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Victor Reinor</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Victor Reinor"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
