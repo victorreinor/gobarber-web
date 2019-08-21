a47949a74540fc2121d293246fbc49d2d14bf117
Victor Reinor <victorreinor@gmail.com>
2019-08-21 20:20:49 -0300

    adiciona codigo com problema

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,20 +4,24 @@ import { Form, Input } from '@rocketseat/unform';
 
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
+import AvatarInput from './AvatarInput';
+
 import { Container } from './styles';
 
 function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  const dispatch = useDispatch();
-
   function handleSubmit(data) {
-    dispatch(updateProfileRequest(data));
+    console.log('data,', data);
+    // dispatch(updateProfileRequest(data));
   }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input type="name" name="name" placeholder="Nome Completo" />
         <Input type="email" name="email" placeholder="Seu endereço de e-mail" />
         <hr />
