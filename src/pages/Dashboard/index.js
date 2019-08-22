cd129ed711e311c0d9c8dafee4850b2faf4a734e
Victor Reinor <victorreinor@gmail.com>
2019-08-22 19:53:49 -0300

    adiciona estrutura e estilizacao da dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
+
 function Dashboard() {
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Victor Reinor</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Victor Reinor</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
 
